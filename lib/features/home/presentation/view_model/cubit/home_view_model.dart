c0ba4ec0c33dd515be272515cb17cf4f1a0bdad4
Marwan Sayed <marwan.sayed.soliman95@gmail.com>
2026-03-02 00:10:19 +0200

    Delete Images Paths File Create Dummy file Remove comments in HomeViewModel

--- lib/features/home/presentation/view_model/cubit/home_view_model.dart
+++ lib/features/home/presentation/view_model/cubit/home_view_model.dart
@@ -11,7 +11,6 @@ import 'package:injectable/injectable.dart';
 class HomeViewModel extends Cubit<HomeState> {
   HomeViewModel(this._getProductsUseCase, this._getCategoriesUseCase)
     : super(HomeState());
-// Remote Change
   final GetProductsUseCase _getProductsUseCase;
   final GetCategoriesUseCase _getCategoriesUseCase;
 
@@ -28,7 +27,6 @@ class HomeViewModel extends Cubit<HomeState> {
         break;
     }
   }
-//Remote change 222
   Future<void> _getAllData() async {
     await Future.wait([_getProducts1(), _getProducts2()]);
   }
